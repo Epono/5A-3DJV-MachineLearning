2f441fcf7f5747b06522046193747744e80ff126
Epono <guillaume.ambrois@gmail.com>
2016-04-15 13:59:42 +0200

    Scène Unity Ok

--- MachineLearningUnity/Assets/Scripts/DigitsScript.cs
+++ MachineLearningUnity/Assets/Scripts/DigitsScript.cs
@@ -33,42 +33,13 @@ public class DigitsScript : MonoBehaviour {
     [DllImport("LearningDllForUnity")]
     public extern static void LinearPerceptronRegression_Deletion(System.IntPtr index);
 
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     List<PictureScript> examples = new List<PictureScript>();
     List<PictureScript> tests= new List<PictureScript>();
 
     public void LoadFiles() {
-
-        List<double> a = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a1 = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a2 = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a3 = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a4 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a5 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a6 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a7 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a8 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a9 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a10 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a11 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a12 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a13 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a14 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a15 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a16 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a17 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a18 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a19 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a20 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a21 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a22 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a23 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a24 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a25 = new List<double>() { 0, 100, 200, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a26 = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        List<double> a27 = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        List<List<double>> l = new List<List<double>>() { a, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27 };
-
         // Lecture des exemples pour l'entrainement
         DateTime start = DateTime.Now;
         StreamReader reader = new StreamReader(File.OpenRead(@"Assets\Data\train_light.csv"));
@@ -103,8 +74,12 @@ public class DigitsScript : MonoBehaviour {
         Debug.Log("La lecture du fichier train_light.csv a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
         start = DateTime.Now;
 
-        for(int i = 0; i < 10; ++i) {
+        for(int i = 0; i < 1000; ++i) {
             examples.Add(new PictureScript(pixelColors[i], 28, digits[i], true, new Vector3((i % 100) - 10, 0, 5 - (int)(i / 100))));
+        }
+
+        for(int i = 1000; i < 2000; ++i) {
+            tests.Add(new PictureScript(pixelColors[i], 28, digits[i], true, new Vector3((i % 100) - 10, 0, (5 - (int)(i / 100)) - 1)));
         }
         end = DateTime.Now;
         Debug.Log("La création des scripts a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
@@ -112,44 +87,41 @@ public class DigitsScript : MonoBehaviour {
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Lecture des test pour la verification
-        start = DateTime.Now;
-        reader = new StreamReader(File.OpenRead(@"Assets\Data\test_light.csv"));
+        //start = DateTime.Now;
+        //reader = new StreamReader(File.OpenRead(@"Assets\Data\test_light.csv"));
 
-        // On lit la 1ère avec les labels pour s'en débarasser
-        headerLine = reader.ReadLine();
+        //// On lit la 1ère avec les labels pour s'en débarasser
+        //headerLine = reader.ReadLine();
 
-        // Liste des digits
-        digits = new List<int>();
-        // Liste des couleurs de pixel
-        pixelColors = new List<List<double>>();
+        //// Liste des digits
+        //digits = new List<int>();
+        //// Liste des couleurs de pixel
+        //pixelColors = new List<List<double>>();
 
-        int testsCount = 0;
+        //int testsCount = 0;
 
-        while(!reader.EndOfStream) {
-            string line = reader.ReadLine();
-            string[] values = line.Split(',');
+        //while(!reader.EndOfStream) {
+        //    string line = reader.ReadLine();
+        //    string[] values = line.Split(',');
 
-            // Le digit est le 1er caractère
-            digits.Add(int.Parse(values[0]));
+        //    // On remplit une liste de double avec les valeurs du pixel
+        //    List<double> datas = new List<double>();
+        //    for(int i = 0; i < values.Length; ++i) {
+        //        datas.Add(double.Parse(values[i]));
+        //    }
+        //    pixelColors.Add(datas);
 
-            // On remplit une liste de double avec les valeurs du pixel
-            List<double> datas = new List<double>();
-            for(int i = 1; i < values.Length; ++i) {
-                datas.Add(double.Parse(values[i]));
-            }
-            pixelColors.Add(datas);
+        //    ++testsCount;
+        //}
+        //end = DateTime.Now;
+        //Debug.Log("La lecture du fichier test_light.csv a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
 
-            ++testsCount;
-        }
-        end = DateTime.Now;
-        Debug.Log("La lecture du fichier test_light.csv a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
-
-        start = DateTime.Now;
-        for(int i = 0; i < 10; ++i) {
-            tests.Add(new PictureScript(pixelColors[i], 28, digits[i], true, new Vector3((i % 100) - 10, -10, 5 - (int)(i / 100))));
-        }
-        end = DateTime.Now;
-        Debug.Log("La création des scripts a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
+        //start = DateTime.Now;
+        //for(int i = 0; i < 1000; ++i) {
+        //    tests.Add(new PictureScript(pixelColors[i], 28, 0, true, new Vector3((i % 100) - 10, -10, 5 - (int)(i / 100))));
+        //}
+        //end = DateTime.Now;
+        //Debug.Log("La création des scripts a pris : " + ((end - start).TotalMilliseconds) / 1000 + " s");
     }
 
     public void Train_Digit() {
@@ -210,8 +182,12 @@ public class DigitsScript : MonoBehaviour {
             // on affiche ce qu'on a décidé
             for(int i = 0; i < 10; ++i) {
                 //Debug.Log(" i = " + i + ": " + results[i]);
-                if(results[i] == 1 && i == ps.GetDigit) {
-                    numberOfSuccessExamples++;
+                if(results[i] == 1) {
+                    ps.SetPredictedValue(i);
+                    if(i == ps.GetDigit) {
+                        numberOfSuccessExamples++;
+                    }
+                    break;
                 }
             }
         }
@@ -235,8 +211,12 @@ public class DigitsScript : MonoBehaviour {
             // on affiche ce qu'on a décidé
             for(int i = 0; i < 10; ++i) {
                 //Debug.Log(" i = " + i + ": " + results[i]);
-                if(results[i] == 1 && i == ps.GetDigit) {
-                    numberOfSuccessTests++;
+                if(results[i] == 1) {
+                    ps.SetPredictedValue(i);
+                    if(i == ps.GetDigit) {
+                        numberOfSuccessTests++;
+                    }
+                    break;
                 }
             }
         }
@@ -250,38 +230,34 @@ public class DigitsScript : MonoBehaviour {
         //}
     }
 
-    public void Test_Digit() {
-        // Calcul de Eout (predict sur le jeu de test)
-        double Eout;
-        int numberOfSuccessTests = 0;
-        // Pour chaque image
-        foreach(PictureScript ps in tests) {
+    bool _isShowingTrueValue = false;
+    bool _isShowingPredictedValue = false;
 
-            double[] inputs = ps.GetPixelsColor.ToArray();
-
-            // Pour chaque model
-            for(int i = 0; i < 10; ++i) {
-                //results.Add(i, LinearPerceptronClassification_Predict(models[i], 28 * 28, inputs));
-            }
-
-            // on affiche ce qu'on a décidé
-            for(int i = 0; i < 10; ++i) {
-                //Debug.Log(" i = " + i + ": " + results[i]);
-                //if(results[i] == 1 && i == ps.GetDigit) {
-                //    numberOfSuccessTests++;
-                //}
-            }
-        }
-
-        Eout = numberOfSuccessTests / tests.Count;
-        Debug.Log("Eout : " + Eout);
-    }
-    bool _isShowingTrueValue= false;
     void Update() {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if(Input.GetKeyDown(KeyCode.A)) {
             _isShowingTrueValue = !_isShowingTrueValue;
             examples.ForEach(s => s.ShowTrueValue(_isShowingTrueValue));
+            tests.ForEach(s => s.ShowTrueValue(_isShowingTrueValue));
+        } else if(Input.GetKeyDown(KeyCode.Z)) {
+            _isShowingPredictedValue = !_isShowingPredictedValue;
+            tests.ForEach(s => s.ShowPredictedValue(_isShowingPredictedValue));
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.position -= new Vector3(0.5f, 0, 0);
+        } else if(Input.GetKey(KeyCode.RightArrow)) {
+            gameObject.transform.position += new Vector3(0.5f, 0, 0);
+        } else if(Input.GetKey(KeyCode.UpArrow)) {
+            gameObject.transform.position += new Vector3(0, 0, 0.5f);
+        } else if(Input.GetKey(KeyCode.DownArrow)) {
+            gameObject.transform.position -= new Vector3(0, 0, 0.5f);
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") < 0) {
+            gameObject.GetComponent<Camera>().orthographicSize += 0.1f;
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") > 0) {
+            gameObject.GetComponent<Camera>().orthographicSize -= 0.1f;
         }
     }
 }
